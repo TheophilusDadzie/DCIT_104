30736fa919a9381d2863f75cf18bf1aca69a651c
@Philus <98459253+TheophilusDadzie@users.noreply.github.com>
2022-08-04 14:49:43 +0000

    Add files via upload

---
+++ dcit104/script.js
@@ -0,0 +1,22 @@
+// to find the avg af all even number less than 1000
+// 1. find an arry of all even numbers less than 1000
+// 2. Sum them
+// 2. find the average of them
+
+let even_numbers = [];
+for (let i = 0; i < 10000; i++) {
+    if(i % 2 == 0)
+    {
+        even_numbers.push(i);
+    }
+    
+}
+let sum = 0;
+for (let i = 0; i < even_numbers.length; i++) {
+    sum += i;
+    
+}
+
+console.log(even_numbers);
+let average = sum / even_numbers.length
+console.log("Therefore, the average of all the even number less than 10000 is " + average);
